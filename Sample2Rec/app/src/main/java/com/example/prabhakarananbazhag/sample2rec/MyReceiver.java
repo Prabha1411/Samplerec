3d04a0bba545d42987c0b09404c1be07f5876b31
prabhakaran.anbazhag <prabhakarana2018@siet.ac.in>
2018-05-31 12:02:43 +0530

    Broadcast receiver

---
+++ Sample2Rec/app/src/main/java/com/example/prabhakarananbazhag/sample2rec/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.prabhakarananbazhag.sample2rec;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String broadcastMessage = intent.getStringExtra("broadcast");
+
+        Toast.makeText(context, "Message: " + broadcastMessage,
+
+                Toast.LENGTH_LONG).show();
+    }
+}
